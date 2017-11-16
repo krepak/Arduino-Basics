5d3e517a9334a6dd6419b72cdb957de8b27cb13d
Krepak Ivan <arafoxnis@gmail.com>
2017-11-16 15:00:07 +0600

    Add files via upload

---
+++ BareMinimum/BareMinimum.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+  // тут вводится код, который будет выполняться единажды
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // тут вводится код, который будет работать до выключения Arduino
+
+}
