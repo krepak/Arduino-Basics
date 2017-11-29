cd79baa05cd2c7398a0b3702f17cd21ac33362fe
Krepak Ivan <arafoxnis@gmail.com>
2017-11-29 22:47:51 +0600

    Add files via upload

---
+++ LightDetectorArduino/LightDetectorArduino.ino
@@ -0,0 +1,19 @@
+const int ledPin = 4;
+const int ldrPin = A0;
+ 
+void setup() {
+  pinMode(ledPin, OUTPUT);  
+  pinMode(ldrPin, INPUT);
+}
+ 
+void loop() {
+  int ldrStatus = analogRead(ldrPin);
+  if (ldrStatus <=300) {
+    digitalWrite(ledPin, HIGH);              
+    delay(1000);
+  }
+  else {
+    digitalWrite(ledPin, LOW);  
+  }
+  delay(100);
+}
