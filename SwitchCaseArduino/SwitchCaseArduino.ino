6bd84fa76d58a6c26aba4cf349ba9d102b01a0dc
Krepak Ivan <arafoxnis@gmail.com>
2017-12-01 19:25:27 +0600

    Add files via upload

---
+++ SwitchCaseArduino/SwitchCaseArduino.ino
@@ -0,0 +1,27 @@
+const int sensorMin = 0;      // sensor minimum, discovered through experiment
+const int sensorMax = 600;    // sensor maximum, discovered through experiment
+
+void setup() 
+  Serial.begin(9600);
+}
+
+void loop() {
+  int sensorReading = analogRead(A0);
+  int range = map(sensorReading, sensorMin, sensorMax, 0, 3);
+  
+  switch (range) {
+    case 0:
+      Serial.println("temno");
+      break;
+    case 1:
+      Serial.println("dim");
+      break;
+    case 2:
+      Serial.println("poydet");
+      break;
+    case 3:
+      Serial.println("yarko");
+      break;
+  }
+  delay(1);
+}
